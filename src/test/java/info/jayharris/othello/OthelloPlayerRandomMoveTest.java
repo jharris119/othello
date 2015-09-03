51ecd3d1f6b82463fd5b88b27fd0408ee0be7a0d
Jay Harris <jayharris@gmail.com>
2015-09-03 15:52:05 -0400

    currentField should be static

--- src/test/java/info/jayharris/othello/OthelloPlayerRandomMoveTest.java
+++ src/test/java/info/jayharris/othello/OthelloPlayerRandomMoveTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class OthelloPlayerRandomMoveTest {
 
-    Field currentField;
+    static Field currentField;
 
     public OthelloPlayerRandomMoveTest() throws Exception {
         currentField = Othello.class.getDeclaredField("current");
