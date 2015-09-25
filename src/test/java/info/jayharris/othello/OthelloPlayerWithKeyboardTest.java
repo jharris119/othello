434521d8aceea899299bc7c35bf774d0279c17b4
Jay Harris <jayharris@gmail.com>
2015-09-24 23:07:40 -0400

    added mockito dependency

---
+++ src/test/java/info/jayharris/othello/OthelloPlayerWithKeyboardTest.java
@@ -0,0 +1,16 @@
+package info.jayharris.othello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jayharris on 9/23/15.
+ */
+public class OthelloPlayerWithKeyboardTest {
+
+    @Test
+    public void testGetMove() throws Exception {
+
+    }
+}
